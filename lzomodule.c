01bb7bac877e33a810963a949a436cdd1f5a0444
Stuart Donnan <s.donnan@nasa.gov>
2015-01-09 14:42:01 -0600

    Fixing same bug in the other two places it comes up

--- lzomodule.c
+++ lzomodule.c
@@ -159,7 +159,7 @@ decompress(PyObject *dummy, PyObject *args)
         goto header_error;
     in_len = len - 5;
     out_len = (in[1] << 24) | (in[2] << 16) | (in[3] << 8) | in[4];
-    if ((int)out_len < 0 || in_len > out_len + out_len / 64 + 16 + 3)
+    if ((int)out_len < 0 || in_len > out_len + out_len / 16 + 64 + 3)
         goto header_error;
 
     /* alloc buffers */
@@ -215,7 +215,7 @@ optimize(PyObject *dummy, PyObject *args)
         goto header_error;
     in_len = len - 5;
     out_len = (in[1] << 24) | (in[2] << 16) | (in[3] << 8) | in[4];
-    if ((int)out_len < 0 || in_len > out_len + out_len / 64 + 16 + 3)
+    if ((int)out_len < 0 || in_len > out_len + out_len / 16 + 64 + 3)
         goto header_error;
 
     /* alloc buffers */
